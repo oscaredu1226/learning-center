0dc68dd8f8d50ba6ab5e8fc855a704d18e2661d4
Oscar <oscaredu01@hotmail.com>
2024-09-18 21:11:27 -0500

    chore: added fake api data and configuration files.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'ACME Learning Center',
+    };
+  },
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
